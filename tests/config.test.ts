42ae74bd8f672c9f8ab02400979879a1249b60c3
mgamal <mgamal@kashier.io>
2023-05-13 18:32:02 +0300

    fix config test: fr this time

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -4,12 +4,12 @@ import { existsSync } from "fs";
 describe("CaptchaConfigManager", () => {
   describe("default", () => {
     it("returns a config object", () => {
-      expect(CaptchaConfigManager.default("")).toHaveProperty("fontPath");
+      expect(CaptchaConfigManager.default()).toHaveProperty("fontPath");
     });
 
     it("returns a valid default font path", () => {
       expect(
-        existsSync(CaptchaConfigManager.default("").fontPath)
+        existsSync(CaptchaConfigManager.default().fontPath)
       ).toBeTruthy();
     });
   });
