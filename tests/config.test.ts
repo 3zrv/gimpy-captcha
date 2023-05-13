1e72e6a7ddcb45bbb19c4dbd54d64111fb1cf45d
mgamal <mgamal@kashier.io>
2023-05-13 18:29:17 +0300

    fix config test

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -4,11 +4,13 @@ import { existsSync } from "fs";
 describe("CaptchaConfigManager", () => {
   describe("default", () => {
     it("returns a config object", () => {
-      expect(CaptchaConfigManager.default('', '')).toHaveProperty("fontPath");
+      expect(CaptchaConfigManager.default("")).toHaveProperty("fontPath");
     });
-    
+
     it("returns a valid default font path", () => {
-      expect(existsSync(CaptchaConfigManager.default('', '').fontPath)).toBeTruthy()
+      expect(
+        existsSync(CaptchaConfigManager.default("").fontPath)
+      ).toBeTruthy();
     });
   });
 });
