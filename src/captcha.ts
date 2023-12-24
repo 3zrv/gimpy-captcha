0e6ee46fc19fe96ef4546c4a100f3f73703cc2f1
Mohamed Gamal <3zrv@pm.me>
2023-12-24 16:23:10 +0200

    feat: add random tiny characters to the captcha

--- src/captcha.ts
+++ src/captcha.ts
@@ -1,121 +1,116 @@
-import { ICaptchaConfig } from "./config";
-import { CaptchaCodeExpression } from "./expressions/code-expression";
-import { ICaptchaExpression } from "./expressions/types";
-import { renderText } from "./font";
-import { CaptchaMathExpression } from "./expressions/math-expression";
-import * as random from "./random";
-import {
-  decrypt,
-  keyToBuffer,
-  verifySignature,
-  encryptAndSign,
-} from "./crypto";
-import * as errors from "./errors";
+import { ICaptchaConfig } from './config'
+import { CaptchaCodeExpression } from './expressions/code-expression'
+import { ICaptchaExpression } from './expressions/types'
+import { renderText } from './font'
+import { CaptchaMathExpression } from './expressions/math-expression'
+import * as random from './random'
+import { decrypt, keyToBuffer, verifySignature, encryptAndSign } from './crypto'
+import * as errors from './errors'
 
 export type ICaptcha = {
   /**
    * An unencrypted representation of the captcha
    */
-  expr: string;
+  expr: string
   /**
    * An unencrypted string representation of the captcha
    */
-  encryptedExpr: string;
+  encryptedExpr: string
   /**
    * Captcha SVG
    */
-  captchaSvg: string;
+  captchaSvg: string
 
   /**
    * Unix timestamp when the captcha expires (UTC)
    */
-  validUntil: number;
-};
+  validUntil: number
+}
 
 export function create(config: ICaptchaConfig): ICaptcha {
-  let captcha: ICaptchaExpression;
+  let captcha: ICaptchaExpression
 
   switch (config.mode) {
-    case "code":
-      captcha = CaptchaCodeExpression.generate(config.codeLength);
-      break;
-    case "math":
-      captcha = CaptchaMathExpression.generate(2);
-      break;
+    case 'code':
+      captcha = CaptchaCodeExpression.generate(config.codeLength)
+      break
+    case 'math':
+      captcha = CaptchaMathExpression.generate(2)
+      break
     default:
-      throw new Error(`unknown captcha mode ${config.mode}`);
+      throw new Error(`unknown captcha mode ${config.mode}`)
   }
 
-  const timestamp = Date.now() + config.captchaDuration;
+  const timestamp = Date.now() + config.captchaDuration
   const jsonToEncrypt = JSON.stringify({
     validUntil: timestamp,
     captcha: captcha.toObject(),
-  });
+  })
 
   return {
     expr: jsonToEncrypt,
     encryptedExpr: encryptAndSign(
       jsonToEncrypt,
       config.cryptoKey,
-      config.signatureKey
+      config.signatureKey,
     ),
     captchaSvg: renderCaptcha(captcha, config),
     validUntil: timestamp,
-  };
+  }
 }
 
 export function verify(
   encryptedExpression: string,
   solution: string,
   key: string,
-  signatureKey: string
+  signatureKey: string,
 ) {
-  let captcha: ICaptchaExpression & { type: string }, validUntil: number;
+  let captcha: ICaptchaExpression & { type: string }, validUntil: number
   try {
     const message = verifySignature(
       encryptedExpression,
-      keyToBuffer(signatureKey)
-    );
+      keyToBuffer(signatureKey),
+    )
     // 1. Verify signature
     if (!message) {
-      return errors.INVALID_DATA;
+      return errors.INVALID_DATA
     }
 
-    const decrypted = decrypt(message, keyToBuffer(key));
-    const parsed = JSON.parse(decrypted);
+    const decrypted = decrypt(message, keyToBuffer(key))
+    const parsed = JSON.parse(decrypted)
 
-    captcha = parsed.captcha;
-    validUntil = parsed.validUntil;
+    captcha = parsed.captcha
+    validUntil = parsed.validUntil
   } catch (e) {
-    console.error(e);
-    return errors.INVALID_DATA;
+    console.error(e)
+    return errors.INVALID_DATA
   }
 
-  const currentTimestamp = Date.now();
+  const currentTimestamp = Date.now()
   if (validUntil <= currentTimestamp) {
-    return errors.CAPTCHA_EXPIRED;
+    return errors.CAPTCHA_EXPIRED
   }
 
   switch (captcha.type) {
-    case "code":
+    case 'code':
       {
-        const codeExpression = CaptchaCodeExpression.fromJSON(captcha);
+        const codeExpression = CaptchaCodeExpression.fromJSON(captcha)
         if (codeExpression.solve() === solution) {
-          return true;
+          return true
         }
       }
-      return errors.INVALID_SOLUTION;
-    case "math":
+      return errors.INVALID_SOLUTION
+    case 'math':
       {
-        const mathExpression = CaptchaMathExpression.fromJSON(captcha);
+        const mathExpression = CaptchaMathExpression.fromJSON(captcha)
 
         if (mathExpression.solve().toString() === solution) {
-          return true;
+          return true
         }
       }
-      return errors.INVALID_SOLUTION;
+      return errors.INVALID_SOLUTION
     default:
-      throw new Error(`unknown captcha type ${captcha.type}`);
+      throw new Error(`unknown captcha type ${captcha.type}`)
   }
 }
 
@@ -127,21 +122,21 @@ export function verify(
  */
 function renderCaptcha(
   expression: ICaptchaExpression,
-  config: ICaptchaConfig
+  config: ICaptchaConfig,
 ): string {
-  let background = "";
-  let noise = "";
-  const start = `<svg xmlns="http://www.w3.org/2000/svg" width="${150}" height="${80}" viewBox="0,0,${150},${80}">`;
+  let background = ''
+  let noise = ''
+  const start = `<svg xmlns="http://www.w3.org/2000/svg" width="${150}" height="${80}" viewBox="0,0,${150},${80}">`
 
   if (config.backgroundColor) {
-    background = `<rect width="100%" height="100%" fill="${config.backgroundColor}"/>`;
+    background = `<rect width="100%" height="100%" fill="${config.backgroundColor}"/>`
   }
 
-  noise = renderNoise(config).join("");
+  noise = renderNoise(config).join('')
 
-  const text = renderText(expression.toString(), config);
-  const str = `${start}${background}${text}${noise}</svg>`;
-  return str;
+  const text = renderText(expression.toString(), config)
+  const str = `${start}${background}${text}${noise}</svg>`
+  return str
 }
 
 /**
@@ -151,61 +146,81 @@ function renderCaptcha(
  * @returns {string[]} An array of SVG path strings for the rendered noise lines and/or circles.
  */
 function renderNoise(options: ICaptchaConfig): string[] {
-  // If noise is disabled, return an empty array
-  if (!options.noise) return [];
+  if (!options.noise) return []
 
-  const { width, height } = options;
-  const hasColor = options.backgroundColor;
-  const noiseShapes = [];
-  const minSize = 2;
-  const maxSize = 6;
+  const { width, height } = options
+  const hasColor = options.backgroundColor
+  const noiseShapes = []
+  const minSize = 2
+  const maxSize = 6
 
-  // Generate the specified number of noise shapes
   for (let i = 0; i < options.noise; i++) {
-    let shape;
+    let shape
 
-    if (Math.random() < 0.5) {
+    const randomValue = Math.random()
+
+    if (randomValue < 0.25) {
       // Generate a random vertical line segment using Bezier curves
-      const start = `${random.int(1, width - 1)} ${random.int(1, 21)}`;
+      const start = `${random.int(1, width - 1)} ${random.int(1, 21)}`
       const end = `${random.int(1, width - 1)} ${random.int(
         height - 21,
-        height - 1
-      )}`;
+        height - 1,
+      )}`
       const mid1 = `${random.int(1, width - 1)} ${random.int(
         height / 4,
-        height / 2
-      )}`;
+        height / 2,
+      )}`
       const mid2 = `${random.int(1, width - 1)} ${random.int(
         height / 2,
-        (height * 3) / 4
-      )}`;
+        (height * 3) / 4,
+      )}`
       const color = hasColor
         ? random.color(hasColor)
-        : random.greyColor(minSize, maxSize);
-      shape = `<path d="M${start} C${mid1},${mid2},${end}" stroke="${color}" fill="none"/>`;
-    } else {
+        : random.greyColor(minSize, maxSize)
+      shape = `<path d="M${start} C${mid1},${mid2},${end}" stroke="${color}" fill="none"/>`
+    } else if (randomValue < 0.5) {
       // Generate a random horizontal line segment using Bezier curves
-      const start = `${random.int(1, width - 21)} ${random.int(1, height - 1)}`;
+      const start = `${random.int(1, width - 21)} ${random.int(1, height - 1)}`
       const end = `${random.int(width - 21, width - 1)} ${random.int(
         1,
-        height - 1
-      )}`;
+        height - 1,
+      )}`
       const mid1 = `${random.int(width / 4, width / 2)} ${random.int(
         1,
-        height - 1
-      )}`;
+        height - 1,
+      )}`
       const mid2 = `${random.int(width / 2, (width * 3) / 4)} ${random.int(
         1,
-        height - 1
-      )}`;
+        height - 1,
+      )}`
       const color = hasColor
         ? random.color(hasColor)
-        : random.greyColor(minSize, maxSize);
-      shape = `<path d="M${start} C${mid1},${mid2},${end}" stroke="${color}" fill="none"/>`;
+        : random.greyColor(minSize, maxSize)
+      shape = `<path d="M${start} C${mid1},${mid2},${end}" stroke="${color}" fill="none"/>`
+    } else if (randomValue < 0.75) {
+      // Generate a lined circle
+      const cx = random.int(1, width - 1)
+      const cy = random.int(1, height - 1)
+      const radius = random.int(minSize, maxSize)
+      const color = hasColor
+        ? random.color(hasColor)
+        : random.greyColor(minSize, maxSize)
+      shape = `<circle cx="${cx}" cy="${cy}" r="${radius}" stroke="${color}" fill="none"/>`
+    } else {
+      const character = random.tinyCharacter()
+      const x = random.int(1, width - 1)
+      const y = random.int(1, height - 1)
+      const color = hasColor
+        ? random.color(hasColor)
+        : random.greyColor(minSize, maxSize)
+      shape = `<text x="${x}" y="${y}" fill="${color}" font-size="${random.int(
+        8,
+        12,
+      )}">${character}</text>`
     }
 
-    noiseShapes.push(shape);
+    noiseShapes.push(shape)
   }
 
-  return noiseShapes;
+  return noiseShapes
 }
--- src/random.ts
+++ src/random.ts
@@ -1,57 +1,57 @@
 function hslToRgb(hsl: string) {
   // Parse the HSL values from the input string
   const [hue, saturation, lightness] = hsl
-    .replace(/hsla?|\(|\)/g, "")
-    .split(",")
-    .map((v: string) => parseFloat(v));
+    .replace(/hsla?|\(|\)/g, '')
+    .split(',')
+    .map((v: string) => parseFloat(v))
 
   // Convert the hue to a value between 0 and 360 (in degrees)
-  const h = ((hue % 360) + 360) % 360;
+  const h = ((hue % 360) + 360) % 360
 
   // Convert the saturation and lightness to values between 0 and 1
-  const s = saturation / 100;
-  const l = lightness / 100;
+  const s = saturation / 100
+  const l = lightness / 100
 
   // Calculate the chroma (color intensity) and secondary components
-  const c = (1 - Math.abs(2 * l - 1)) * s;
-  const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
-  const m = l - c / 2;
+  const c = (1 - Math.abs(2 * l - 1)) * s
+  const x = c * (1 - Math.abs(((h / 60) % 2) - 1))
+  const m = l - c / 2
 
   // Calculate the RGB values based on the hue and chroma
-  let r, g, b;
+  let r, g, b
   if (h < 60) {
-    r = c;
-    g = x;
-    b = 0;
+    r = c
+    g = x
+    b = 0
   } else if (h < 120) {
-    r = x;
-    g = c;
-    b = 0;
+    r = x
+    g = c
+    b = 0
   } else if (h < 180) {
-    r = 0;
-    g = c;
-    b = x;
+    r = 0
+    g = c
+    b = x
   } else if (h < 240) {
-    r = 0;
-    g = x;
-    b = c;
+    r = 0
+    g = x
+    b = c
   } else if (h < 300) {
-    r = x;
-    g = 0;
-    b = c;
+    r = x
+    g = 0
+    b = c
   } else {
-    r = c;
-    g = 0;
-    b = x;
+    r = c
+    g = 0
+    b = x
   }
 
   // Add the secondary components and convert the values to 8-bit integers
-  r = Math.round((r + m) * 255);
-  g = Math.round((g + m) * 255);
-  b = Math.round((b + m) * 255);
+  r = Math.round((r + m) * 255)
+  g = Math.round((g + m) * 255)
+  b = Math.round((b + m) * 255)
 
   // Return the RGB values as an array of integers
-  return [r, g, b];
+  return [r, g, b]
 }
 
 /**
@@ -62,13 +62,13 @@ function hslToRgb(hsl: string) {
  * @returns {number} A random integer between `min` and `max`.
  */
 export function randomInt(min: number, max: number): number {
-  return Math.round(min + Math.random() * (max - min));
+  return Math.round(min + Math.random() * (max - min))
 }
 
 /**
  * Alias for `randomInt`.
  */
-export const int = randomInt;
+export const int = randomInt
 
 /**
  * Generates a random shade of gray in hexadecimal format (#RRGGBB) based on the input range.
@@ -80,8 +80,8 @@ export const int = randomInt;
 export function greyColor(min = 1, max = 9): string {
   const intensity = Math.floor((randomInt(min, max) * 255) / 15)
     .toString(16)
-    .padStart(2, "0");
-  return `#${intensity}${intensity}${intensity}`;
+    .padStart(2, '0')
+  return `#${intensity}${intensity}${intensity}`
 }
 
 /**
@@ -93,26 +93,26 @@ export function greyColor(min = 1, max = 9): string {
  */
 export function color(bgColor?: string): string {
   // Generate a random hue value between 0 and 360 (in degrees)
-  const hue = Math.floor(Math.random() * 360);
+  const hue = Math.floor(Math.random() * 360)
 
   // Generate a random saturation value between 60% and 80%
-  const saturation = randomInt(60, 80);
+  const saturation = randomInt(60, 80)
 
   // Calculate the lightness range based on the input background color
-  const bgLightness = bgColor ? getLightness(bgColor) : 1.0;
-  const range = bgLightness >= 0.5 ? [-45, -25] : [25, 45];
+  const bgLightness = bgColor ? getLightness(bgColor) : 1.0
+  const range = bgLightness >= 0.5 ? [-45, -25] : [25, 45]
 
   // Generate a random lightness value within the calculated range
   const lightness = (
     Math.round(bgLightness * 100 + randomInt(range[0], range[1])) / 100
-  ).toFixed(2);
+  ).toFixed(2)
 
   // Calculate the RGB values using the HSL-to-RGB conversion formula
-  const hsl = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-  const rgb = hslToRgb(hsl);
+  const hsl = `hsl(${hue}, ${saturation}%, ${lightness}%)`
+  const rgb = hslToRgb(hsl)
 
   // Convert the RGB values to hexadecimal format and return the color value
-  return `#${rgb.map((c) => c.toString(16).padStart(2, "0")).join("")}`;
+  return `#${rgb.map(c => c.toString(16).padStart(2, '0')).join('')}`
 }
 
 /**
@@ -123,17 +123,17 @@ export function color(bgColor?: string): string {
  * @returns {number} `-1.0` if the input color is not a valid 6-digit hexadecimal color code.
  */
 export function getLightness(rgbColor: string): number {
-  const hexColor = rgbColor.startsWith("#") ? rgbColor.slice(1) : null;
+  const hexColor = rgbColor.startsWith('#') ? rgbColor.slice(1) : null
 
-  if (!hexColor || !/^[0-9A-Fa-f]{6}$/.test(hexColor)) return -1.0;
+  if (!hexColor || !/^[0-9A-Fa-f]{6}$/.test(hexColor)) return -1.0
 
-  const r = parseInt(hexColor.substring(0, 2), 16);
-  const g = parseInt(hexColor.substring(2, 4), 16);
-  const b = parseInt(hexColor.substring(4, 6), 16);
-  const max = Math.max(r, g, b);
-  const min = Math.min(r, g, b);
+  const r = parseInt(hexColor.substring(0, 2), 16)
+  const g = parseInt(hexColor.substring(2, 4), 16)
+  const b = parseInt(hexColor.substring(4, 6), 16)
+  const max = Math.max(r, g, b)
+  const min = Math.min(r, g, b)
 
-  return (max + min) / (2 * 255);
+  return (max + min) / (2 * 255)
 }
 
 /**
@@ -146,15 +146,27 @@ export function getLightness(rgbColor: string): number {
  */
 
 export function hue2rgb(p: number, q: number, h: number): number {
-  const t = (h + 1) % 1;
+  const t = (h + 1) % 1
   if (6 * t < 1) {
-    return p + (q - p) * 6 * t;
+    return p + (q - p) * 6 * t
   }
   if (2 * t < 1) {
-    return q;
+    return q
   }
   if (3 * t < 2) {
-    return p + (q - p) * (2 / 3 - t) * 6;
+    return p + (q - p) * (2 / 3 - t) * 6
   }
-  return p;
+  return p
+}
+
+/**
+ * Generates a random lowercase letter or digit.
+ *
+ * @returns {string} A random lowercase letter or digit.
+ */
+
+export function tinyCharacter(): string {
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  const randomIndex = Math.floor(Math.random() * characters.length)
+  return characters[randomIndex]
 }
